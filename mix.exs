b393d52e4d9d10815d0823b6a318d7e059a74fd9
Isaac Yung <isaaczoi@gmail.com>
2018-02-17 14:50:41 -0200

    Add deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Blockchain.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: Coverex.Task]
     ]
   end
 
@@ -21,8 +22,11 @@ defmodule Blockchain.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:earmark, "~> 1.2.3"},
+      {:ex_doc, "~> 0.16.2", only: :dev, runtime: false},
+      {:exsync, "~> 0.2.1", only: :dev},
+      {:coverex, "~> 1.4", only: [:test, :dev]},
+      {:credo, "~> 0.8.4", only: [:dev, :test]}
     ]
   end
 end
