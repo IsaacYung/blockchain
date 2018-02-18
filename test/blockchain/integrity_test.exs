079f63ba20cd0d504b8b4575f0015d25115bbbee
Isaac Yung <isaaczoi@gmail.com>
2018-02-18 16:40:07 -0300

    Tests

--- test/blockchain/integrity_test.exs
+++ test/blockchain/integrity_test.exs
@@ -3,6 +3,7 @@ defmodule Blockchain.IntegrityTest do
   doctest Blockchain
   alias Blockchain.Chain
   alias Blockchain.Integrity
+  alias Blockchain.Block
 
   setup do
     {:ok, server_pid} = Chain.start_link
@@ -28,6 +29,17 @@ defmodule Blockchain.IntegrityTest do
     Blockchain.add_block("Genesis block 4", state[:server])
     Blockchain.add_block("Genesis block 5", state[:server])
 
-    assert Integrity.valid_blockchain(Chain.queue) == false
+    chain = Chain.queue
+    fail_block = %Block{
+      index: 9,
+      data: "Genesis block attack",
+      timestamp: "2018-02-17 18:07:39.770360Z",
+      previous_hash: "Van0+JlLG78gWOo4yO+2xFkACBTV85wIcAJXFjnmIw4=",
+      hash: "YDA64iuZiGG847KPM+7BvnWKITyGyTwHbb6fVYwRx1I="
+    }
+
+    fail_chain = chain ++ [fail_block]
+
+    assert Integrity.valid_blockchain(fail_chain) == false
   end
 end
